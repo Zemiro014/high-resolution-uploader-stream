1f3518639a79ca83db4b9278ed3e26a3a1f214f7
Zemiro014 <zemiro014@gmail.com>
2026-04-04 20:37:31 -0300

    frontend created

---
+++ frontend/src/api/storageService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.VITE_API_URL
+});
+
+export const storageService = {
+      // 1. Pede a URL de upload
+  getPresignedUrl: (fileName, fileType) => 
+    api.post('/storage/generate-upload-url', { fileName, fileType }),
+
+  // 2. Faz o PUT direto para o S3
+  uploadToS3: (url, file) => 
+    axios.put(url, file, { headers: { 'Content-Type': file.type } }),
+
+  // 3. Confirma o sucesso no seu Backend
+  confirmUpload: (fileId) => 
+    api.patch(`/storage/confirm-upload/${fileId}`)
+}
