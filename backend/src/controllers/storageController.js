dd7463b5394f9ead02a7cb0fe57ec59d9a19728b
Zemiro014 <zemiro014@gmail.com>
2026-04-04 20:08:49 -0300

    set up prisma and creating API to confirm upload

--- backend/src/controllers/storageController.js
+++ backend/src/controllers/storageController.js
@@ -1,9 +1,11 @@
 const { GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 const s3Client = require("../config/S3");
+const { PrismaClient } = require("../../prisma/generated/client")
+const prisma = new PrismaClient();
 
 exports.getUploadUrl = async (req, res) => {
-    const { fileName, fileType, size, description } = req.body;
+    const { fileName, fileType } = req.body;
 
     if(!fileName || !fileType){
         return res.status(400).json({error: "Insufficient Data"})
@@ -11,16 +13,14 @@ exports.getUploadUrl = async (req, res) => {
 
     const fileKey = `uploads/${Date.now()}-${fileName}`;
 
-    const dbRecord = {
-        id: Math.floor(Math.random() * 1000),
-        fileName,
-        fileKey,
-        fileType,
-        size,
-        description,
-        status: "PENDIND",
-        createdAt: new Date()
-    };
+    const fileRecord = await prisma.file.create({
+        data: {
+            fileName,
+            fileKey,
+            fileType,
+            status: "PENDIND",
+        }
+    });
 
     const command = new PutObjectCommand({
         Bucket: process.env.Bucket_Name,
@@ -32,11 +32,26 @@ exports.getUploadUrl = async (req, res) => {
         const uploadUrl = await getSignedUrl(s3Client, command, { expiresIn: 300});
         res.json({
             uploadUrl,
-            fileId: dbRecord.id,
+            fileId: fileRecord.id,
             fileKey
         });
     } catch (err) {
         console.log(err)
         res.status(500).json({error: "Error to get Upload URL"});
     }
-}
+};
+
+exports.confirmUpload = async (req, res) => {
+  const { fileId } = req.params;
+
+  try {
+    const updatedFile = await prisma.file.update({
+      where: { id: Number(fileId) },
+      data: { status: "UPLOADED" } // Ou "SUCCESS" como preferir
+    });
+
+    res.json({ message: "Upload confirmado com sucesso!", file: updatedFile });
+  } catch (err) {
+    res.status(404).json({ error: "Arquivo não encontrado ou erro na atualização" });
+  }
+};
--- backend/src/routers/storageRoutes.js
+++ backend/src/routers/storageRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const storageController = require("../controllers/storageController");
 
 router.post("/generate-upload-url", storageController.getUploadUrl);
+router.patch("/confirm-upload/:fileId", storageController.confirmUpload);
 
 module.exports = router;
