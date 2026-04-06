a16f75788b32b11e8ec115167abb24073b80e4d2
Zemiro014 <zemiro014@gmail.com>
2026-04-06 17:00:44 -0300

    S3 config: credentials removed

--- backend/src/config/S3.js
+++ backend/src/config/S3.js
@@ -2,11 +2,7 @@ const {S3Client} = require("@aws-sdk/client-s3");
 require('dotenv').config();
 
 const s3Client = new S3Client({
-    region: process.env.Region || "us-east-1",
-    credentials: {
-        accessKeyId: process.env.Access_Key_ID,
-        secretAccessKey: process.env.Secret_Access_Key,
-    },
+    region: process.env.Region || "us-east-1"
 });
 
 module.exports = s3Client;
