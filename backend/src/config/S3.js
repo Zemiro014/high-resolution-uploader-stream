9fe06a7793c7b9de2d5fac5af894f1d18966e42e
Zemiro014 <zemiro014@gmail.com>
2026-04-06 16:46:36 -0300

    adjustments in backend environment and in template

--- backend/src/config/S3.js
+++ backend/src/config/S3.js
@@ -2,7 +2,7 @@ const {S3Client} = require("@aws-sdk/client-s3");
 require('dotenv').config();
 
 const s3Client = new S3Client({
-    region: process.env.Region,
+    region: process.env.Region || "us-east-1",
     credentials: {
         accessKeyId: process.env.Access_Key_ID,
         secretAccessKey: process.env.Secret_Access_Key,
